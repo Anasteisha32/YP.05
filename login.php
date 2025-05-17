74db667dbda181e3a9cbc1e3d4747499938c784d
Anasteisha32 <165366553+Anasteisha32@users.noreply.github.com>
2025-05-17 12:11:27 +0500

    Страница авторизации

---
+++ login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MAL - Авторизация</title>
+    <link rel="stylesheet" href="styles.css">
+    <style>
+        .logo {
+    font-size: 34px;
+    font-weight: bold;
+    color: rgb(78, 102, 34);
+    margin-bottom: 20px;
+}
+        .btn-primary {
+    background-color: var(--secondary-color);
+    color: var(--text-color);
+    border: 1px solid var(--border-color);
+    width: 337px;
+   height: 40px;
+            border-radius: 20px;
+    padding: 3px;
+}
+.btn-primary:hover {
+    background-color: var(--primary-dark);
+    color: white;
+}
+    </style>
+</head>
+<body>
+    <div class="auth-container">
+        <div class="logo">MAL - система управления проектами</div>
+        <div class="auth-form">
+            <h2>Вход в систему</h2>
+            <form>
+                <div class="form-group">
+                    <label for="email">Электронная почта</label>
+                    <input type="email" id="email" placeholder="Введите email" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Пароль</label>
+                    <input type="password" id="password" placeholder="Введите пароль" required>
+                </div>
+                <button type="submit" class="btn-primary">Войти</button>
+            </form>
+            <div class="auth-footer">
+            <p>Нет аккаунта? <a href="register.php">Зарегистрироваться</a></p>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
